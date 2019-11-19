30d3a54987a3c4a6c6b61879edeb882af591735f
qinghe <iwangqinghe@gmail.com>
2019-11-19 10:22:13 +0800

    字面量

--- src/standard/es5/literal.ts
+++ src/standard/es5/literal.ts
@@ -1,18 +1,26 @@
+
+// tslint:disable-next-line: no-console
+const log = console.log;
 export const Literal = {
     RegExpLiteral(path) {
         const { node } = path;
+        log('regexp literal', path)
         return new RegExp(node.pattern, node.flags);
     },
     StringLiteral(path) {
+        log('string literral', path)
         return path.node.value;
     },
     NumericLiteral(path) {
+        log ('number literial', path)
         return path.node.value;
     },
     BooleanLiteral(path) {
+        log('boolean literial', path)
         return path.node.value;
     },
     NullLiteral(path) {
+        log('null literal', null)
         return null;
     }
 }
